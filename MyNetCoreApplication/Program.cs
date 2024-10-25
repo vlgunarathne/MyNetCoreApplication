da4c5f0e49aed44593d4b7b30b2f156d7439a3a6
Viduranga <vlgunarathne@gmail.com>
2024-10-25 15:42:25 +1100

    Implement dependency injection

--- MyNetCoreApplication/Program.cs
+++ MyNetCoreApplication/Program.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Rewrite;
-using Microsoft.VisualBasic;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<ITaskService>(new InMemoryTaskService());
 var app = builder.Build();
 
 // Middlewares
@@ -25,19 +25,18 @@ app.MapGet("/", () => "Hello World!");
 
 var toDos = new List<ToDo>();
 
-app.MapGet("/todos", () => toDos);
+app.MapGet("/todos", (ITaskService service) => service.GetToDos());
 
-app.MapGet("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id) =>
+app.MapGet("/todos/{id}", Results<Ok<ToDo>, NotFound> (int id, ITaskService service) =>
 {
     Console.WriteLine("Getting a single todo.");
-    var targetToDo = toDos.SingleOrDefault(t => id == t.ID);
+    var targetToDo = service.GetToDoById(id);
     return targetToDo is null ? TypedResults.NotFound() : TypedResults.Ok(targetToDo);
 });
 
-app.MapPost("/todos", (ToDo task) =>
+app.MapPost("/todos", (ToDo task, ITaskService service) =>
 {
-    toDos.Add(task);
-    return TypedResults.Created("/todos/{id}", task);
+    return TypedResults.Created("/todos/{id}", service.AddToDo(task));
 })
 .AddEndpointFilter(async (context, next) =>
 {
@@ -60,12 +59,45 @@ app.MapPost("/todos", (ToDo task) =>
     return await next(context);
 });
 
-app.MapDelete("/todos/{id}", (int id) =>
+app.MapDelete("/todos/{id}", (int id, ITaskService service) =>
 {
-    toDos.RemoveAll(t => id == t.ID);
+    service.DeleteTodoById(id);
     return TypedResults.NoContent();
 });
 
 app.Run();
 
 public record ToDo(int ID, string Name, DateTime DueDate, bool IsCompleted);
+
+interface ITaskService
+{
+    ToDo? GetToDoById(int id);
+
+    List<ToDo> GetToDos();
+
+    void DeleteTodoById(int id);
+
+    ToDo AddToDo(ToDo newTodo);
+}
+
+class InMemoryTaskService : ITaskService
+{
+    private readonly List<ToDo> _todos = [];
+    public ToDo AddToDo(ToDo newTodo)
+    {
+        _todos.Add(newTodo);
+        return newTodo;
+    }
+
+    public void DeleteTodoById(int id)
+    {
+        _todos.RemoveAll(t => t.ID == id);
+    }
+
+    public ToDo? GetToDoById(int id) => _todos.SingleOrDefault(t => t.ID == id);
+
+    public List<ToDo> GetToDos()
+    {
+        return _todos;
+    }
+}
--- MyNetCoreApplication/obj/Debug/net8.0/MyNetCoreApplication.AssemblyInfo.cs
+++ MyNetCoreApplication/obj/Debug/net8.0/MyNetCoreApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNetCoreApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea3acb450f740ed7bfa96ee4303f1197a3da5c4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNetCoreApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNetCoreApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
